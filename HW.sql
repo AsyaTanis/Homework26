f1e635d1d837aa4296395569731a63dafd320aed
Анастасия <tasyaosipova736@gmail.com>
2023-04-12 20:31:42 +0300

    Выборка данных и вложенные запросы

--- HW.sql
+++ HW.sql
@@ -1,3 +1,4 @@
+/* Прошлое дз */
 CREATE DATABASE skypro;
 CREATE TABLE employee (
                           id BIGSERIAL  NOT NULL PRIMARY KEY,
@@ -24,3 +25,49 @@ DELETE FROM employee WHERE id = 1;
 
 SELECT * FROM employee;
 
+/* Задание 1 */
+
+INSERT INTO employee (first_name, last_name, gender, age)
+VALUES ('Иван', 'Иванов', 'муж', 34);
+INSERT INTO employee (first_name, last_name, gender, age)
+VALUES ('Екатерина', 'Иванова', 'жен', 30);
+INSERT INTO employee (first_name, last_name, gender, age)
+VALUES ('Андрей', 'Иванов', 'муж', 34);
+
+SELECT first_name AS Имя, last_name AS Фамилия
+FROM employee;
+
+SELECT * FROM employee
+WHERE age < 30 OR age > 50;
+
+SELECT * FROM employee
+WHERE age BETWEEN 30 AND 50;
+
+SELECT first_name AS Имя, last_name AS Фамилия
+FROM employee
+ORDER BY last_name ASC;
+
+SELECT first_name AS Имя, last_name AS Фамилия
+FROM employee
+WHERE first_name LIKE '%_____%';
+
+/* Задание 2 */
+
+UPDATE employee SET first_name = 'Андрей', last_name = 'Петров', gender = 'муж', age = 31 WHERE id = 4;
+UPDATE employee SET first_name = 'Анастасия', last_name = 'Иванова', gender = 'жен', age = 24 WHERE id = 3;
+
+SELECT first_name AS Имя,
+       SUM(age) AS Суммарный_возраст
+FROM employee
+GROUP BY first_name;
+
+SELECT first_name,
+       MIN (age) AS Min_возраст
+FROM employee
+GROUP BY first_name;
+
+SELECT first_name,
+       MAX(age) AS Max_возраст
+FROM employee
+GROUP BY first_name HAVING COUNT(*)>1
+ORDER BY MAX(age)
