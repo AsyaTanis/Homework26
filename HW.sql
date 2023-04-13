174c1a7d679764d27f247c74d11cd6f0479c5676
Анастасия <tasyaosipova736@gmail.com>
2023-04-13 13:32:26 +0300

    Работа с несколькими таблицами

--- HW.sql
+++ HW.sql
@@ -1,4 +1,4 @@
-/* Прошлое дз */
+/* Из ррошлого дз */
 CREATE DATABASE skypro;
 CREATE TABLE employee (
                           id BIGSERIAL  NOT NULL PRIMARY KEY,
@@ -8,66 +8,64 @@ CREATE TABLE employee (
                           age INT NOT NULL
 );
 
+/*Задание 1*/
+CREATE TABLE city (
+                      city_id INT PRIMARY KEY,
+                      city_name VARCHAR(50) NOT NULL
+);
+
+SELECT * FROM city;
+SELECT * FROM employee;
+
+ALTER TABLE employee
+    ADD city_id INT,
+    ADD FOREIGN KEY (city_id)  REFERENCES city (city_id);
+
+INSERT INTO city (city_id, city_name) VALUES
+                                          (1, 'Москва'),
+                                          (2, 'Сочи'),
+                                          (3, 'Рязань');
+
+UPDATE employee SET city_id = 1 WHERE id = 4;
+UPDATE employee SET city_id = 3 WHERE id = 2;
+UPDATE employee SET city_id = 2 WHERE id = 3;
+UPDATE employee SET city_id = 2 WHERE id = 6;
+UPDATE employee SET city_id = 1 WHERE id = 5;
+
+
+/*Задание 2*/
 INSERT INTO employee (first_name, last_name, gender, age)
 VALUES ('Анатолий', 'Иванов', 'муж', 34);
-INSERT INTO employee (first_name, last_name, gender, age)
-VALUES ('Сергей', 'Иванов', 'муж', 34);
-INSERT INTO employee (first_name, last_name, gender, age)
-VALUES ('Геннадий', 'Иванов', 'муж', 34);
 
-SELECT * FROM employee;
+INSERT INTO city (city_id, city_name) VALUES (4, 'Минск');
 
-UPDATE employee SET first_name = 'Анастасия', last_name = 'Исянова', gender = 'жен', age = 24 WHERE id = 2;
-
-SELECT * FROM employee;
-
-DELETE FROM employee WHERE id = 1;
-
-SELECT * FROM employee;
-
-/* Задание 1 */
-
-INSERT INTO employee (first_name, last_name, gender, age)
-VALUES ('Иван', 'Иванов', 'муж', 34);
-INSERT INTO employee (first_name, last_name, gender, age)
-VALUES ('Екатерина', 'Иванова', 'жен', 30);
-INSERT INTO employee (first_name, last_name, gender, age)
-VALUES ('Андрей', 'Иванов', 'муж', 34);
-
-SELECT first_name AS Имя, last_name AS Фамилия
-FROM employee;
-
-SELECT * FROM employee
-WHERE age < 30 OR age > 50;
-
-SELECT * FROM employee
-WHERE age BETWEEN 30 AND 50;
-
-SELECT first_name AS Имя, last_name AS Фамилия
+SELECT employee.first_name AS Имя, employee.last_name AS Фамилия, city.city_name AS Город
 FROM employee
-ORDER BY last_name ASC;
+INNER  JOIN city
+ON employee.city_id = city.city_id;
 
-SELECT first_name AS Имя, last_name AS Фамилия
+SELECT city.city_name AS Город,
+       employee.first_name AS Имя,
+       employee.last_name AS Фамилия
+FROM city
+LEFT JOIN employee
+ON city.city_id = employee.city_id;
+
+SELECT employee.first_name AS Имя,
+       employee.last_name AS Фамилия,
+       city.city_name AS Город
 FROM employee
-WHERE first_name LIKE '%_____%';
+FULL OUTER JOIN city
+ON employee.city_id = city.city_id;
 
-/* Задание 2 */
-
-UPDATE employee SET first_name = 'Андрей', last_name = 'Петров', gender = 'муж', age = 31 WHERE id = 4;
-UPDATE employee SET first_name = 'Анастасия', last_name = 'Иванова', gender = 'жен', age = 24 WHERE id = 3;
-
-SELECT first_name AS Имя,
-       SUM(age) AS Суммарный_возраст
+SELECT employee.first_name AS Имя,
+       employee.last_name AS Фамилия,
+       city.city_name AS Город
 FROM employee
-GROUP BY first_name;
+CROSS JOIN city;
 
-SELECT first_name,
-       MIN (age) AS Min_возраст
-FROM employee
-GROUP BY first_name;
-
-SELECT first_name,
-       MAX(age) AS Max_возраст
-FROM employee
-GROUP BY first_name HAVING COUNT(*)>1
-ORDER BY MAX(age);
+SELECT city.city_name AS Город
+FROM city
+LEFT JOIN employee
+ON city.city_id = employee.city_id
+WHERE employee.city_id IS NULL;
